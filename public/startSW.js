8565892685148fd5ebdec71eb248d64145fd6a2d
ishaank17 <ishaankkamath@gmail.com>
2025-06-04 00:37:18 +0530

    pwa

--- public/startSW.js
+++ public/startSW.js
@@ -6,3 +6,19 @@ if('serviceWorker' in navigator) {
 }else{
     console.log("NOT THERE");
 }
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    console.log('beforeinstallprompt event fired');
+});
+
+document.getElementById("download").addEventListener('click', async () => {
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const choice = await deferredPrompt.userChoice;
+        console.log('User choice:', choice.outcome);
+        deferredPrompt = null;
+    }
+}, { once: true });
