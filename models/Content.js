178a01cb4c33fe259639fdc473fb6d64b7940343
ishaank17 <ishaankkamath@gmail.com>
2025-06-05 01:39:37 +0530

    ch8

--- models/Content.js
+++ models/Content.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const MONGO_URL = process.env.MONGO_URL;
+const DB_NAME=process.env.DB_NAME;
 const mongoose = require('mongoose');
-mongoose.connect(`${MONGO_URL}`,{dbName: 'SemStore'})
+mongoose.connect(`${MONGO_URL}`,{dbName: `${DB_NAME}`})
 const contentSchema = mongoose.Schema({
     coursecode:String,
     title:String,
