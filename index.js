29e8cd483a3855c855f73d30b95f6af64214462a
ishaank17 <ishaankkamath@gmail.com>
2025-06-06 02:05:16 +0530

    ch11

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const apikeys= {
     "type": "service_account",
     "project_id": "oauth-461019",
     "private_key_id": `${process.env.STORAGE_KEY_ID}`,
-    "private_key": process.env.STORAGE_KEY.split(String.raw`\n`).join('\n'),
+    "private_key": process.env.STORAGE_KEY.replace(/\\n/g, '\n'),
     "client_email": `${process.env.STORAGE_CLIENT_EMAIL}`,
     "client_id":process.env.STORAGE_CLIENT_ID ,
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
